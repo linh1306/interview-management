db04d64cf1435bf9a8a7d03bda2e87e51c165fee
Trịnh Đức Hoàng <124485427+DK625@users.noreply.github.com>
2024-12-29 17:45:49 +0700

    change server ip

--- automation_test/job.py
+++ automation_test/job.py
@@ -65,8 +65,8 @@ class TestJob:
 
     def login(self, username='admin', password='123123'):
         """Login to application"""
-        self.page.goto("http://localhost:5173/login")
-        # self.page.goto("http://103.56.158.135:5173/login")
+        # self.page.goto("http://localhost:5173/login")
+        self.page.goto("http://103.56.158.135:5173/login")
         self.page.fill("input[placeholder='Username']", username)
         self.page.fill("input[placeholder='Password']", password)
         self.page.click("button[type='submit']")
